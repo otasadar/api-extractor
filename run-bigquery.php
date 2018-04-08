22773c6209cf15b0f48a03fd07c505fdfe6de28d
pujoljulia-annalect <julia.ardevol@annalect.com>
2018-04-08 10:24:15 +0400

    Facebook split by date added

--- run-bigquery.php
+++ run-bigquery.php
@@ -8,7 +8,6 @@ $helpers = new helpers();
 
 //Refresh access token
 $access_token = $helpers->get_access_token('1072155568501-rpcfah9qpkg9jro4c6c9sh62go6pm7oe.apps.googleusercontent.com', 'PAx5fz386w0groUL8JFgdVuQ', '1/T32mKg5ITADEUXA5viQuDBNyg8yhDuIyCO3z6Mqy2cM');
-$bucket = $extractions['global']['google_storage']['bucket'];
 
 syslog(LOG_DEBUG, 'ACCESS TOKEN-> ' . $access_token);
 
@@ -18,24 +17,29 @@ $headers = array('content-type: application/json', 'authorization : Bearer ' . $
 //End point
 $api_version = $extractions['global']['google_bigquery']['api_version'];
 $endpoint = "https://www.googleapis.com/bigquery/$api_version/projects/annalect-api-jobs/jobs?alt=json";
-$schema = file_get_contents("gs://$bucket/config/{$_GET['schema']}" );
+$list_query_projects = file_get_contents('https://storage.googleapis.com/annalect-dashboarding/config/queries.json');
+echo json_decode($list_query_projects,true)[$_GET['projectId']]['query'];
 
 //Payload data
 $payload = '{
-	"configuration": {
-		"load": {
-			"destinationTable": {
+    "configuration": {
+        "query": {
+            "query": "' . json_decode($list_query_projects,true)[$_GET['projectId']]['query'] . '",
+            "allowLargeResults": true,
+            "destinationTable": {
 				"tableId": "' . $_GET['tableId'] . '",
 				"projectId": "annalect-api-jobs",
 				"datasetId": "' . $_GET['datasetId'] . '"
-			},
-			"skipLeadingRows": 1,
-			"writeDisposition" : "WRITE_TRUNCATE",
-	        "schema": { '.$schema.'},
-			"sourceUris": ["gs://'.$bucket.'/' . $_GET['filePath'] . '"]
-		}
-	}
+            },
+        "defaultDataset": {
+            "datasetId": "annalect_dashboarding",
+            "projectId": "annalect-api-jobs"
+        },
+        "writeDisposition": "WRITE_TRUNCATE"
+    }
+  }
 }';
+
 
 echo "<pre>";
 echo $payload;
@@ -47,5 +51,4 @@ syslog(LOG_DEBUG, 'PAYLOAD-> ' . json_decode(json_encode($payload)));
 //CURL request
 $curl_response = $helpers->set_curl($headers, $endpoint, $payload, 'POST', null);
 
-syslog(LOG_DEBUG, 'curl response-> '.json_encode($curl_response, JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT));
-
+syslog(LOG_DEBUG, 'curl response-> ' . json_encode($curl_response, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
